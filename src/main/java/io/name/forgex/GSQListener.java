9eed8e25086a0cd7634f7435a21e7974466dc065
GSQLin <57855470+GSQLin@users.noreply.github.com>
2022-12-29 14:26:47 +0800

    Update GSQListener.java

--- src/main/java/io/name/forgex/GSQListener.java
+++ src/main/java/io/name/forgex/GSQListener.java
@@ -3,9 +3,12 @@ package me.gsqlin.pretendpoke;
 import com.pixelmonmod.pixelmon.api.events.FishingEvent;
 
 import static me.gsqlin.pretendpoke.PixelUtil.forgeEventMethod;
-public class GSQListener {
-
-    public static void play(Object event) throws Exception {
-        if (forgeEventMethod.invoke(event) instanceof FishingEvent.Reel);
+public class GSQListener implements Listener{
+    @EventHandler
+    public void onMyForgeEvent(MyForgeEvent event)throws Exception{
+        if (event.getForgeEvent() instanceof PokeLootEvent.GetDrops) {
+            PokeLootEvent.GetDrops e = (PokeLootEvent.GetDrops) event.getForgeEvent();
+            //监听打开精灵奖励箱事件
+        }
     }
 }
